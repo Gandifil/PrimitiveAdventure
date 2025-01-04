9e6c3a5b7f49ef7bea4b7f5c70b776b8aec52450
gandifil <gandifilmk@gmail.com>
2025-01-04 19:21:46 +0300

    Add printing global map cell

--- Screens/GlobalModeScreen.cs
+++ Screens/GlobalModeScreen.cs
@@ -1,14 +1,17 @@
 ﻿using PrimitiveAdventure.Core;
+using SadConsole.Components;
 
 namespace PrimitiveAdventure.Screens;
 
-public class GlobalModeScreen: ScreenSurface
+public class GlobalModeScreen: Console 
 {
     private readonly GameState _gameState;
     
     public GlobalModeScreen(GameState gameState) : base(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
     {
         _gameState = gameState;
+        
+        Cursor.PrintAppearanceMatchesHost = false;
         
         RenderMap(_gameState.GlobalMap);
     }
@@ -21,9 +24,18 @@ public class GlobalModeScreen: ScreenSurface
         for (int x = 0; x < map.Size.X; x++)
         for (int y = 0; y < map.Size.Y; y++)
         {
-            Surface.DrawBox(new Rectangle(CELL_WIDTH * x, CELL_HEIGHT * y, CELL_WIDTH + 1, CELL_HEIGHT + 1), 
+            var rect = new Rectangle(CELL_WIDTH * x, CELL_HEIGHT * y, CELL_WIDTH + 1, CELL_HEIGHT + 1);
+            Surface.DrawBox(rect, 
                 ShapeParameters.CreateStyledBoxThin(Color.Aqua));
-            
+
+            var cell = map[x, y];
+            if (cell is not null)
+            {
+                Cursor
+                    .SetPrintAppearance(Color.Yellow)
+                    .Move(rect.X + 1, rect.Y + 1)
+                    .Print(cell.Name);
+            }
         }
         Surface.ConnectLines(ICellSurface.ConnectedLineThin);
     }
