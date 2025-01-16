42d9047c93618fb845b3f03fa71161935a202ae5
gandifil <gandifilmk@gmail.com>
2025-01-16 22:11:15 +0300

    Add credits for game

--- Screens/Credits.cs
+++ Screens/Credits.cs
@@ -1,8 +1,29 @@
-﻿namespace PrimitiveAdventure.Screens;
+﻿using System.Reflection;
 
-public class Credits: ScreenSurface
+namespace PrimitiveAdventure.Screens;
+
+public class Credits: Console
 {
     public Credits() : base(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT-1)
     {
+        string[] text =
+        {
+            "Name : Primitive Adventure",
+            string.Empty,
+            $"Version : {Assembly.GetExecutingAssembly().GetName().Version}",
+            string.Empty,
+            "Made by : gandifil, gandifilmk@gmail.com",
+            string.Empty,
+            "Made for www.igdc.ru"
+        };
+        var typingInstruction = new SadConsole.Instructions.DrawString(SadConsole.ColoredString.Parser.Parse(string.Join("\r\n", text)));
+        typingInstruction.TotalTimeToPrint = TimeSpan.FromSeconds(4); // 0.5 seconds per line of text
+
+        Cursor.Position = new Point(1, 1);
+        Cursor.IsEnabled = false;
+        Cursor.IsVisible = true;
+        typingInstruction.Cursor = Cursor;
+
+        SadComponents.Add(typingInstruction);
     }
 }
