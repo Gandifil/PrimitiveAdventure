3eec89c312290092d5dba43bd8f0f92cbb11daaf
gandifil <gandifilmk@gmail.com>
2025-01-30 22:12:58 +0300

    Add using ControlCursor for MainMenu.

--- PrimitiveAdventure/Screens/MainMenu.cs
+++ PrimitiveAdventure/Screens/MainMenu.cs
@@ -1,4 +1,5 @@
 ﻿using PrimitiveAdventure.SadConsole;
+using PrimitiveAdventure.SadConsole.Controls;
 using PrimitiveAdventure.Screens.Base;
 using PrimitiveAdventure.Screens.Views;
 using SadConsole.UI.Controls;
@@ -7,33 +8,22 @@ namespace PrimitiveAdventure.Screens;
 
 public class MainMenu: GlobalScreen
 {
-    private readonly Button _button;
-    private readonly TextBox _textBox;
- 
     public MainMenu()
     {
-        _button = new Button("Click me2!");
-        _button.Position = new Point(10, 5);
-        //_button.Text = "Click me!";
-        Controls.Add(_button);
-        
-        _button = new Button("Об игре".Prepare());
-        _button.Position = new Point(10, 6);
-        //_button.Text = "Click me!";
-        _button.Click += (_, __) =>
+        var credit = new Button("Об игре".Prepare());
+        credit.Click += (_, _) =>
         {
             new BackScreen<Credits>((_, _) => this.Start()).Start();
         };
-        Controls.Add(_button);
         
-        _button = new Button("Exit");
-        _button.Position = new Point(11, 7);
-        //_button.Text = "Click me!";
-        _button.Click += (_, __) => Environment.Exit(0);
-        Controls.Add(_button);
- 
-        _textBox = new TextBox(width: 30);
-        _textBox.Position = new Point(10, 8);
-        Controls.Add(_textBox);
+        var exit = new Button("Выход".Prepare());
+        exit.Click += (_, _) => Environment.Exit(0);
+
+        Controls.GetControlCursor()
+            .ToStart()
+            .NewLine()
+            .SetShift(1)
+            .Print(credit)
+            .Print(exit);
     }
 }
