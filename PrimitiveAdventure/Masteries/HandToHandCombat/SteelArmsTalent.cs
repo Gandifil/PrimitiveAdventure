722dd7dd3cadec3821e6dded3f1a78e794daedf3
gandifil <gandifilmk@gmail.com>
2025-01-30 21:45:13 +0300

    Add masteries instances

---
+++ PrimitiveAdventure/Masteries/HandToHandCombat/SteelArmsTalent.cs
@@ -0,0 +1,16 @@
+﻿using PrimitiveAdventure.Core.Rpg.Masteries;
+using PrimitiveAdventure.Core.Rpg.Modifiers;
+
+namespace PrimitiveAdventure.Masteries.HandToHandCombat;
+
+public class SteelArmsTalent: ITalent
+{
+    public string Name => "Стальные кулаки";
+    public string Description => "Стальные кулаки";
+    public int MaxLevel => 3;
+
+    public ModifiersList GetModifiersList(int level) => new()
+    {
+        
+    };
+}
---
+++ PrimitiveAdventure/Masteries/Swordsmanship/CounterattackSkillTalent.cs
@@ -0,0 +1,12 @@
+﻿using PrimitiveAdventure.Core.Rpg.Masteries;
+using PrimitiveAdventure.Core.Rpg.Modifiers;
+
+namespace PrimitiveAdventure.Masteries.Swordsmanship;
+
+public class CounterattackSkillTalent: ITalent
+{
+    public string Name => "Counterattack";
+    public string Description => "Counterattack skill talent";
+    public int MaxLevel => 3;
+    public ModifiersList GetModifiersList(int level) => new ModifiersList();
+}
