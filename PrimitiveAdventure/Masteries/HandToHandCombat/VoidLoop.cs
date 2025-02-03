ecf160ee0b7d1242b4ee380a28044e0f5f3b4548
gandifil <gandifilmk@gmail.com>
2025-02-03 21:54:50 +0300

    Fix balance for VoidLoop

--- PrimitiveAdventure/Masteries/HandToHandCombat/VoidLoop.cs
+++ PrimitiveAdventure/Masteries/HandToHandCombat/VoidLoop.cs
@@ -11,7 +11,7 @@ public class VoidLoop: ComboAbility
     public VoidLoop()
     {
         Name = "Петля Пустоты";
-        Description = "[c:r f:Yellow]Магия - лишь иллюзия. Иллюзии разрываются[c:u 1]\nЗабирает треть магии цели (минимум - 30), восполняет за счет нее запас сил и наносит урон." 
+        Description = "[c:r f:Yellow]Магия - лишь иллюзия. Иллюзии разрываются[c:u 1]\nЗабирает треть магии цели (минимум - 5), восполняет за счет нее запас сил и наносит урон." 
                       + Description;
         Cost = new CostData(Magic: 10, Stamina: 10);
     }
@@ -20,7 +20,7 @@ public class VoidLoop: ComboAbility
     {
         base.Impact(target);
         
-        var magic = Math.Max(30, target!.Magic.Value / 3);
+        var magic = Math.Max(5, target!.Magic.Value / 3);
 
         if (target!.Magic.Value < magic)
             magic = target!.Magic.Value;
